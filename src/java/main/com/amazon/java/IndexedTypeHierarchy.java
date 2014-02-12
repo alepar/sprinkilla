64b3d567a3e79fcc51d741b678a1aef6ee635b0b
Alexey Parfenov <alepar@gmail.com>
2014-02-12 14:42:26 -0800

    store idea's run configurations

--- src/java/main/com/amazon/java/IndexedTypeHierarchy.java
+++ src/java/main/com/amazon/java/IndexedTypeHierarchy.java
@@ -1,6 +1,13 @@
 package com.amazon.java;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class IndexedTypeHierarchy implements MutableTypeHierarchy {
+
+    private final Map<String, Set<ClassDefinition>> parents = new HashMap<>();
+
     @Override
     public void addDefinition(ClassDefinition definition) {
         throw new RuntimeException("parfenov, implement me!");
